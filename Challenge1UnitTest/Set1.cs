b2f5559dd5fe4618566bb1a88fbcaf0d8e13b524
Mohammad Hossein Amri <amrimh@outlook.com>
2015-11-18 02:10:41 +0800

    Initial commit

---
+++ Challenge1UnitTest/Set1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Challenge1UnitTest
+{
+    [TestClass]
+    public class Set1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
