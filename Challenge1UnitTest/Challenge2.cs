8da17bb0dda0ed3d786627673705ca9924d9bbe4
Mohammad Hossein Amri <amrimh@outlook.com>
2015-11-18 03:07:30 +0800

    Challenge-2

---
+++ Challenge1UnitTest/Challenge2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Challenge1UnitTest
+{
+    [TestClass]
+    public class Challenge2
+    {
+        [TestMethod]
+        public void UnequalLengthHex()
+        {
+            CryptoUtility.Xor xor = new CryptoUtility.Xor();
+
+            try
+            {
+                string result = xor.EncryptEqualLengthHex("7F4", "65Ds");
+                Assert.Fail("No Exeption Passed");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Arguments should have same length.", ex.Message);
+            }
+
+        }
+
+
+        [TestMethod]
+        public void IncorrectHex()
+        {
+            CryptoUtility.Xor xor = new CryptoUtility.Xor();
+
+            try
+            {
+                string result = xor.EncryptEqualLengthHex("7HF", "S6F");
+                Assert.Fail("No Exeption Passed");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Additional non-parsable characters are at the end of the string.", ex.Message);
+            }
+
+        }
+
+
+        [TestMethod]
+        public void TestResult()
+        {
+            CryptoUtility.Xor xor = new CryptoUtility.Xor();
+
+            string result = xor.EncryptEqualLengthHex("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965");
+            Assert.AreEqual("746865206b696420646f6e277420706c6179", result);
+
+
+        }
+    }
+}
---
+++ CryptoUtility/Xor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoUtility
+{
+    public class Xor
+    {
+        public string EncryptEqualLengthHex(string firstStringHex, string secondStringHex )
+        {
+            if (firstStringHex.Length!= secondStringHex.Length)
+            {
+                throw new Exception("Arguments should have same length.");
+            }
+
+
+            Utility cryptoUtil = new Utility();
+            byte[] firstHexByte = cryptoUtil.StringToHex(firstStringHex);
+            byte[] secondHexByte = cryptoUtil.StringToHex(secondStringHex);
+            byte[] xored = new byte[firstHexByte.Length];
+            for (int i = 0; i < firstHexByte.Length; i++)
+            {
+                xored[i] = Convert.ToByte(firstHexByte[i] ^ secondHexByte[i]);
+            }
+            return BitConverter.ToString(xored).Replace("-", "").ToLower();
+        }
+    }
+}
